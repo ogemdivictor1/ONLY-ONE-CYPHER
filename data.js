facfac85f3313dd25ec35db2dfad71d24bb8e449
ogemdivictor1 <ogemdivictor1@gmail.com>
2025-10-05 13:55:12 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+module.exports = {
+  version: "1.0.0",
+  author: "Cypher Deals",
+  info: "Basic data file for Cypher bot to prevent startup error",
+  settings: {
+    developer: "Cypher",
+    bot_name: "CYPHER-XD",
+    repo: "https://github.com/your-repo-link"
+  }
+};
